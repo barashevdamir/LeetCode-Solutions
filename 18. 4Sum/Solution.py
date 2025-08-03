32f69adb0bb848e02c350f9ae72593511f0ea88f
Barashev Damir <118205240+barashevdamir@users.noreply.github.com>
2025-08-03 14:58:40 +0300

    Python3 Solution
          Submission Statistics:
          Question #: 18
          Language: Python3
          Runtime: 268 ms
          Runtime percentile: 87.04
          Memory: 17.844 MB
          Memory percentile: 55.56

---
+++ 18. 4Sum/Solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        result = []
+        n = len(nums)
+
+        for i in range(n-3):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                need = target - nums[i] - nums[j]
+                left, right = j + 1, n - 1
+
+                while left < right:
+                    s = nums[left] + nums[right]
+                    if s == need:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                        while left < right and nums[left] == nums[left+1]:
+                            left +=1
+                        while left < right and nums[right] == nums[right-1]:
+                            right -=1
+
+                        left +=1
+                        right -=1
+                    elif s < need:
+                        left += 1
+                    else:
+                        right -= 1
+        return result
