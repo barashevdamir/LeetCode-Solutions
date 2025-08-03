5d9adcacce8ac7a72dc5afc399a5c43c69fbe1b8
Barashev Damir <118205240+barashevdamir@users.noreply.github.com>
2025-08-03 14:40:23 +0300

    Python3 Solution
          Submission Statistics:
          Question #: 775
          Language: Python3
          Runtime: 52 ms
          Runtime percentile: 37.63
          Memory: 19.332 MB
          Memory percentile: 50.50

---
+++ 775. N-ary Tree Preorder Traversal/Solution.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        result = []
+        if root is None:
+            return result
+
+        result.append(root.val)
+        for child in root.children:
+            result += self.preorder(child)
+
+        return result
+        
