e1310ddd5a3ea3f37583ac5238604d01c5011b8a
Barashev Damir <118205240+barashevdamir@users.noreply.github.com>
2025-08-03 12:48:54 +0300

    Python3 Solution
          Submission Statistics:
          Question #: 776
          Language: Python3
          Runtime: 52 ms
          Runtime percentile: 36.89
          Memory: 19.444 MB
          Memory percentile: 23.25

---
+++ 776. N-ary Tree Postorder Traversal/Solution.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        result = []
+        if root is None:
+            return result
+        
+        for child in root.children:
+            result += self.postorder(child)
+        
+        result.append(root.val)
+        return result
+        
